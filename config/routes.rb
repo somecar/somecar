ef6ed82a013300231961dfc9447f940a03bf7c4a
Neodelf <neodelf@gmail.com>
2013-02-14 02:27:45 +0400

    add route welcome/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Somecar::Application.routes.draw do
-  get "index/welcome"
+  #for some route
+  get "welcome/index"
 
   devise_for :users
 
@@ -54,7 +55,7 @@ Somecar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "index#welcome"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
