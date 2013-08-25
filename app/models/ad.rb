69bcc6717353234893de1d72c7891dc64ec3af87
lvl0nax <lvl0nax@gmail.com>
2013-08-25 20:25:11 +0400

    start implementing search and change form views

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,5 +1,15 @@
+# -*- encoding : utf-8 -*-
 class Ad < ActiveRecord::Base
   attr_accessible :automark, :automodel, :bodytype, :city, :color, :customs, :date, 
   :drive, :enginecapacity, :enginetype, :exchange, :mileage, :owner, :power, 
   :price, :region, :state, :transmission, :wheel
+
+  BODYTYPES = {'седан' => 1, 'хэтчбек' => 2, 'купе' => 3, 'универсал' => 4, 'вседорожник' => 5,
+               'пикап' => 6, 'кабриолет' => 7, 'минивен' => 8, 'фургон' => 9, 'кроссовер' => 0}.freeze
+  ENGINETYPES = {'бензин' => 1, 'дизель' => 2, 'гибрид' => 3}.freeze
+  DRIVETYPE = {'передний' => 1, 'задний' => 2, 'полный' => 3}.freeze
+  TRANSMISSION = {'автомат' => 1, 'механика' => 2}.freeze
+  WHEELTYPE = {'левый' => 1, 'правый' => 2}.freeze
+  CUSTOMS = {'не растаможен' => 0, 'растаможен' => 1}.freeze
+
 end
